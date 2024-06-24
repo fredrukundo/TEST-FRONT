1650cf5b12fb63d9103fc7c2f27ee318766cc196
Fred Rukundo <frukundo@e3r3p18.1337.ma>
2024-06-24 12:17:56 +0100

    Mini-Project

--- Mini-Project/src/components/CustomPieChart.jsx
+++ Mini-Project/src/components/CustomPieChart.jsx
@@ -3,34 +3,27 @@ import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 import { Typography, Box } from '@mui/material';
 
 const data = [
-  { name: "DUNNO", value: 45 },
-  { name: 'DUNNO', value: 7 },
-  { name: 'DUNNO', value: 13 },
-  { name: 'DUNNO', value: 16 },
+  {id:0, name: "DUNNO", value: 7 },
+  {id:1, name: "I'm Boring", value: 10 },
+  {id:2, name: 'Money', value: 50 },
+  {id:3, name: 'Fun', value: 10 },
   
 ];
 
-const COLORS = [ '#855CF8','#9B76FA', '#000000','#ACB9FF' ];
+const COLORS = [ '#000000','#ACB9FF', '#855CF8','#9B76FA' ];
 
 const CustomPieChart = () => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <PieChart width={400} height={400}>
+      <PieChart width={400} height={500}>
         <Pie
           data={data}
-          cx={200}
-          cy={200}
-          labelLine={false}
-          label
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
-        <Tooltip />
+       
       </PieChart>
       <Box display="flex" justifyContent="center" mt={2}>
         {data.map((entry, index) => (
--- Mini-Project/src/pages/Home.jsx
+++ Mini-Project/src/pages/Home.jsx
@@ -6,25 +6,30 @@ import CustomPieChart from '../components/CustomPieChart.jsx';
 
 const Home = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <CustomAppBar />
-      <Grid container spacing={3} sx={{ padding: 3 }}>
+    <Box>
+      <CustomAppBar/>
+      <Grid spacing={3} sx={{ padding: 3,}}>
+        <Typography>Home / Reports & Insights</Typography>
+      </Grid>
+      <Grid  spacing={3} sx={{ padding: 3 }} container>
         <Grid item xs={12} md={6}>
-          <Paper sx={{ padding: 2 }}>
-            <Typography variant="h6">Why do you create a startup?</Typography>
+          <Paper sx={{padding:1}}>
+            <Typography variant="h6">Why do you create a startup ?</Typography>
+            <Typography  sx={{ marginBottom: 2, color: 'gray', }}>Trends
+            </Typography>
             <CustomPieChart />
           </Paper>
         </Grid>
         <Grid item xs={12} md={6}>
           <Paper sx={{ padding: 2 }}>
             <Typography variant="h6">Employees</Typography>
-            <EmployeeList />
+            <EmployeeList/>
           </Paper>
         </Grid>
       </Grid>
     </Box>
-    
   );
 };
 
 export default Home;
+
